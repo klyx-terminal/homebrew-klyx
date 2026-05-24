e123f1fd1fdd3a93bc396537252ae02ef565a638
klyx-terminal <klyx-terminal@users.noreply.github.com>
2026-05-24 15:44:36 +0000

    klyx v0.1.41

--- Casks/klyx.rb
+++ Casks/klyx.rb
@@ -1,6 +1,6 @@
 cask "klyx" do
   version "0.1.41"
-  sha256 "cb7f22e0f28f2e384fd33861446247105fea6ccef436134673172f2ac3d5ef5d"
+  sha256 "d5f05b129e69fdcc49cbd5ab29eeeb4ab9b636616be71f00577e690f9f2c6f96"
 
   url "https://github.com/klyx-terminal/homebrew-klyx/releases/download/v0.1.41/Klyx-v0.1.41-arm64.zip"
   name "Klyx"
